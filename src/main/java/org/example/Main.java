2e36b5f0c098c320799d7be4a2a7d0dac6c34d60
NISHANTHR4 <97089406+NISHANTHR4@users.noreply.github.com>
2024-10-30 21:46:55 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,56 +16,7 @@ public class Main {
 
         try {
             JSONObject jsonObject1 = loadingJson(filePath1);
-            JSONObject jsonObject2 = loadingJson(filePath2);
-
-            BigInteger secret1 = SecretConst(jsonObject1);
-            BigInteger secret2 = SecretConst(jsonObject2);
-
-            System.out.println("Secret from Test Case 1: " + secret1);
-            System.out.println("Secret from Test Case 2: " + secret2);
-        } catch (IOException e) {
-            System.err.println("An error occurred while reading the file: " + e.getMessage());
-        }
-    }
-
-    private static JSONObject loadingJson(String filePath) throws IOException {
-        try (FileReader reader = new FileReader(filePath)) {
-            return new JSONObject(new JSONTokener(reader));
-        }
-    }
-
-    private static BigInteger decodeYValue(String value, int base) {
-        return new BigInteger(value, base); // Since values are large i am using BigInteger
-    }
-
-    private static BigInteger SecretConst(JSONObject jsonObject) {
-        int n = jsonObject.getJSONObject("keys").getInt("n");
-        int k = jsonObject.getJSONObject("keys").getInt("k");
-
-        // Assuming we have k roots to determine the polynomial
-        BigInteger[] xValues = new BigInteger[k];
-        BigInteger[] yValues = new BigInteger[k];
-
-        for (int i = 1; i <= k; i++) {
-            JSONObject root = jsonObject.getJSONObject(String.valueOf(i));
-            String base = root.getString("base");
-            String value = root.getString("value");
-            int radix = Integer.parseInt(base);
-
-            xValues[i - 1] = BigInteger.valueOf(i);
-            yValues[i - 1] = decodeYValue(value, radix);
-        }
-
-        //the secret value also might be big, so we are using BigInteger
-        BigInteger secret = calculateSecret(xValues, yValues, k);
-        return secret;
-    }
-
-    private static BigInteger calculateSecret(BigInteger[] xValues, BigInteger[] yValues, int k) {
-
-        BigInteger secret = BigInteger.ZERO;
-
-        for (int i = 0; i < k; i++) {
+         
             BigInteger numerator = BigInteger.ONE;
             BigInteger denominator = BigInteger.ONE;
 
